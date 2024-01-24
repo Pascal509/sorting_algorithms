ce49cd97014e21eb9784aa6568d39f5bbe974502
Ezenagu chinemerem <chinemeremezenagu@gmail.com>
2024-01-24 02:34:19 +0100

    latest

--- sort.h
+++ sort.h
@@ -34,11 +34,13 @@ void quick_sort(int *array, size_t size);
 
 void shell_sort(int *array, size_t size);
 
-/**void cocktail_sort_list(listint_t **list);
+void cocktail_sort_list(listint_t **list);
 
 void counting_sort(int *array, size_t size);
 
-void merge_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
+
+/**void merge_sort(int *array, size_t size);
 
 void heap_sort(int *array, size_t size);
 
